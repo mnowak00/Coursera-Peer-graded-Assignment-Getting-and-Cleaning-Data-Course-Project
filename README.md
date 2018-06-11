ab1bfbc71efeb9231bc43613b2f620db4f23d561
mnowak00 <marius.nowak@gmail.com>
2018-06-11 21:29:05 +0200

    Added all files

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Coursera-Peer-graded-Assignment-Getting-and-Cleaning-Data-Course-Project
+
+## Repository content
+The repository consists of the following files:
+* README.md - text file describing the repo content, problem and solution
+* UCI HAR Dataset - folder containing the raw dataset
+* run_analysis.R - R script processing the multiple datasets into a clean data
+* clean_data.csv - Comma separated values of the clean data
+* summarized_data.csv - Comma separated values of the clean data summarized by average
+for each groups (subject and activity)
+
+## Problem Summary
+The purpose of the project is to clean data collected by wearable device. The data is
+composed of multiple dataset, split into train and test category. The objective of the
+exercise is to combine the various datasets into a clean table selecting mean and
+standard deviation variables. An additional objective is to summarize the average values 
+of variables grouped by subject and by activity.
+
+## Processing
+The run_analysis.R script will read and process the raw dataset by compiling the train and test
+data, selecting only mean and standard deviation calculations and adding descriptive variable names.
+Finally it summarizes the data by the average of the variables for two groups: by subject and by activity.
+There are two outputs generated the the script:
+* a clean data .csv file named "clean_data.csv"
+* a summarized data .csv file of the clean data named "summarized_data.csv"
+
+## Raw data
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data. 
+The sensor signals (accelerometer and gyroscope) were pre-processed by applying noise filters and then sampled in fixed-width sliding windows of 2.56 sec and 50% overlap (128 readings/window). The sensor acceleration signal, which has gravitational and body motion components, was separated using a Butterworth low-pass filter into body acceleration and gravity. The gravitational force is assumed to have only low frequency components, therefore a filter with 0.3 Hz cutoff frequency was used. From each window, a vector of features was obtained by calculating variables from the time and frequency domain. See 'features_info.txt' for more details. 
+The raw dataset includes the following files:
+* 'README.txt'
+* 'features_info.txt': Shows information about the variables used on the feature vector.
+* 'features.txt': List of all features.
+* 'activity_labels.txt': Links the class labels with their activity name.
+* 'train/X_train.txt': Training set.
+* 'train/y_train.txt': Training labels.
+* 'test/X_test.txt': Test set.
+* 'test/y_test.txt': Test labels.
+The following files are available for the train and test data. Their descriptions are equivalent. 
+* 'train/subject_train.txt': Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30. 
+* 'train/Inertial Signals/total_acc_x_train.txt': The acceleration signal from the smartphone accelerometer X axis in standard gravity units 'g'. Every row shows a 128 element vector. The same description applies for the 'total_acc_x_train.txt' and 'total_acc_z_train.txt' files for the Y and Z axis. 
+* 'train/Inertial Signals/body_acc_x_train.txt': The body acceleration signal obtained by subtracting the gravity from the total acceleration. 
+* 'train/Inertial Signals/body_gyro_x_train.txt': The angular velocity vector measured by the gyroscope for each window sample. The units are radians/second. 
--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,81 @@
 ## DATA CLEANING PROJECT
 
+## Loads the dplyr package which will be needed in this script
+library(dplyr)
+
+## **********************************************************************************************
+## Part 1. Merges the training and the test sets to create one data set.
+## **********************************************************************************************
+
+## Reads the training dataset, labels and subjects files into three dataframes
+train_set <- read.table("./UCI HAR Dataset/train/X_train.txt",header=FALSE)
+train_labels <- read.table("./UCI HAR Dataset/train/y_train.txt",header=FALSE)
+train_subject <- read.table("./UCI HAR Dataset/train/subject_train.txt",header=FALSE)
+
+## Merges the training data into one dataframe
+train <- cbind(train_subject,train_labels,train_set)
+
+## Reads the training dataset, labels and subjects files into three dataframes
+test_set <- read.table("./UCI HAR Dataset/test/X_test.txt",header=FALSE)
+test_labels <- read.table("./UCI HAR Dataset/test/y_test.txt",header=FALSE)
+test_subject <- read.table("./UCI HAR Dataset/test/subject_test.txt",header=FALSE)
+
+## Merges the training data into one dataframe
+test <- cbind(test_subject,test_labels,test_set)
+
+## Merges the training and test data
+data <- rbind(train,test)
+
+## Assigns variable names to the train and test data
+## This is done by first reading the features names from the features.txt file in a valid format
+features_names <- read.table("./UCI HAR Dataset/features.txt",header=FALSE)
+valid_features_names <- make.names(names=features_names$V2, unique=TRUE, allow_ = TRUE)
+names(data) <- c("subject","activity",valid_features_names)
+
+## **********************************************************************************************
+## Part 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+## **********************************************************************************************
+
+## Using the dplyr package, selects the subject, activities and appropriate columns mean and std
+## calculated columns. The "." before and after mean and std ensures the selection of only those
+## columns for which mean and std has been calculated
+
+clean_data <- select(data,subject,activity,contains(".mean."),contains(".std."))
+
+## **********************************************************************************************
+## Part 3. Uses descriptive activity names to name the activities in the data set
+## **********************************************************************************************
+
+## Reads the activity labels table storing it into a dataframe
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+
+## Looks up and replaces the activity number with the appropriate label
+clean_data$activity <- activity_labels$V2[match(clean_data$activity,activity_labels$V1)]
+
+## **********************************************************************************************
+## Part 4. Appropriately labels the data set with descriptive variable names.
+## **********************************************************************************************
+
+## Adds descriptive names by replacing the original column names
+names(clean_data)[3:68] <- gsub("^t","Time_",names(clean_data)[3:68])
+names(clean_data)[3:68] <- gsub("^f","Frequency_",names(clean_data)[3:68])
+names(clean_data)[3:68] <- gsub("Acc","_Acceleration",names(clean_data)[3:68])
+names(clean_data)[3:68] <- gsub("Gyro","_Gyroscope",names(clean_data)[3:68])
+names(clean_data)[3:68] <- gsub("Mag","_Magnitude",names(clean_data)[3:68])
+
+## Writes the dataframe into a .csv file
+clean_data %>% write.csv("clean_data.csv")
+
+## **********************************************************************************************
+## Part 5. From the data set in step 4, creates a second, independent tidy data set with the 
+## average of each variable for each activity and each subject.
+## **********************************************************************************************
+
+## Groups by subject and activities, then summarizes the dataframe by averages of variables
+summarized_data <- clean_data %>% group_by(subject,activity) %>% summarize_all(funs(mean))
+
+## Writes the dataframe into a .csv file
+summarized_data %>% write.csv("summarized_data.csv")
+
+
 
